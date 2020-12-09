7bf1f30943ed950a79947172e7b202cfe410d0ac
Xujuan.Chen <xujuan.chen@lc.cuny.edu>
2020-12-09 09:25:55 -0500

    create api and reducer folder

---
+++ client/src/reducers/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import {createStore, applyMiddleware, compose} from 'redux'
+import thunk from 'redux-thunk'
+import App from './App'
+
+ReactDOM.render(<App />, document.getElementById('root'))
