b775579db1f30956441697c09a2397ef61303a33
Xujuan.Chen <xujuan.chen@lc.cuny.edu>
2020-12-09 08:44:17 -0500

    made navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,34 @@
 import React from 'react'
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core'
+import cubic from './images/cubic.png'
+import PostContainer from './componets/PostContainer/PostContainer'
+import Form from './componets/Form/Form'
+import useStyles from './styles'
 
 const App = () => {
+    const classes = useStyles();
+
     return (
-        <div>
-            App
-        </div>
+        <Container maxWidth="lg">
+            <AppBar className={classes.AppBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">
+                    <img className={classes.image} src={cubic} alt="memories" width="60" height="60"/>
+                    <span></span>Memory Board
+                </Typography>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <PostContainer/>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form/>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     )
 }
 
---
+++ client/src/componets/PostContainer/PostContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Post from '../Post/Post'
+
+const PostContainer = () => {
+    return (
+        <div>
+            PostContainer
+            <Post/>
+            <Post/>
+        </div>
+    )
+}
+
+export default PostContainer
