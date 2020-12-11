595ab9f85993dfd56549487adb2756c4c50ce0e2
Xujuan.Chen <xujuan.chen@lc.cuny.edu>
2020-12-11 11:25:19 -0500

    heroku

--- server/index.js
+++ server/index.js
@@ -3,10 +3,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
@@ -14,11 +15,15 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
+app.get('/',(req, res)=>{
+  res.send('Hello, Memory API');
+})
 
-const CONNECTION_URL = 'mongodb+srv://javascript:mern@cluster1.dcyu7.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
+//const CONNECTION_URL = 'mongodb+srv://javascript:mern@cluster1.dcyu7.mongodb.net/<dbname>?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
